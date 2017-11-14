bcc6145697c0c453997284ffeb117ca9ffd2c49e
yany2 <yuan.yan@dianrong.com>
2017-11-14 18:19:43 +0800

    pc housedetail

--- pc/assets/js/form.js
+++ pc/assets/js/form.js
@@ -1,5 +1,15 @@
 $().ready(function() {
-    $("#forget").validate({
+  jQuery.validator.addMethod("isMobile", function(value, element) {
+    var length = value.length;
+    var mobile = /^(((13[0-9]{1})|(15[0-9]{1}))+\d{8})$/;
+    return this.optional(element) || (length == 11 && mobile.test(value));
+  }, "请正确填写您的手机号码");
+
+  $("#regForm").validate({
+        errorPlacement: function(error, element) {
+          console.log(element)
+          error.appendTo(element.parent());
+        },
         rules: {
             mobile: {
                 required: true,
@@ -19,7 +29,11 @@ $().ready(function() {
             }
         }
     });
-    $("#login").validate({
+    $("#loginForm").validate({
+        errorPlacement: function(error, element) {
+          console.log(element)
+          error.appendTo(element.parent());
+        },
         rules: {
             mobile: {
                 required: true,
@@ -28,6 +42,13 @@ $().ready(function() {
             password: {
                 required: true,
                 minlength: 6
+            },
+            code: {
+              required: true,
+            },
+            phone: {
+              required: true,
+              isMobile: true
             }
         },
         messages: {
@@ -38,33 +59,74 @@ $().ready(function() {
             password: {
                 required: "请输入密码",
                 minlength: "密码长度不能小于 6 个字符！"
-            }
+            },
+            code: {
+              required: "请输入验证码",
+            },
+            phone: {
+              required: "请输入手机号",
+              isMobile:"请输入正确的手机号！"
+            },
         }
     });
-    $("#reset").validate({
+    $("#registerForm").validate({
+        errorPlacement: function(error, element) {
+          console.log(element)
+          error.appendTo(element.parent());
+        },
         rules: {
-            password: {
-                required: true,
-                minlength: 6
-            },
-            confirm_password: {
-                required: true,
-                minlength: 6,
-                equalTo: "#password"
-            },
+          loginPhone: {
+            required: true,
+            isMobile: true
+          },
+          loginPsw: {
+            required: true,
+            minlength: 6
+          },
+          loginCode: {
+            required: true,
+          },
         },
         messages: {
-            password: {
-                required: "请输入密码",
-                minlength: "密码长度不能小于 6 个字符"
-            },
-            confirm_password: {
-                required: "请输入密码",
-                minlength: "密码长度不能小于 6 个字符",
-                equalTo: "两次密码输入不一致"
-            },
+          loginPhone: {
+            required: "请输入手机号",
+            isMobile:"请输入正确的手机号！"
+          },
+          loginPsw: {
+            required: "请输入密码",
+            minlength: "密码长度不能小于 6 个字符！"
+          },
+          loginCode: {
+            required: "请输入验证码",
+          },
         }
     });
+
+  $("#resetForm").validate({
+    errorElement: "span",
+    rules: {
+      password: {
+        required: true,
+        minlength: 6
+      },
+      confirm_password: {
+        required: true,
+        minlength: 6,
+        equalTo: "#password"
+      },
+    },
+    messages: {
+      password: {
+        required: "请输入密码",
+        minlength: "密码长度不能小于 6 个字符"
+      },
+      confirm_password: {
+        required: "请输入密码",
+        minlength: "密码长度不能小于 6 个字符",
+        equalTo: "两次密码输入不一致"
+      },
+    }
+  });
 
     $("#reserve").validate({
         rules: {
